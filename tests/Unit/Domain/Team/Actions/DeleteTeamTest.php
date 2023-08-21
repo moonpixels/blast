7e5c7e2426e84bebabb747cfcc2d759fc11b4576
Adam Hainsworth-Potter <adam@moonpixels.co.uk>
2023-08-21 22:09:31 +0100

    Test

--- tests/Unit/Domain/Team/Actions/DeleteTeamTest.php
+++ tests/Unit/Domain/Team/Actions/DeleteTeamTest.php
@@ -27,7 +27,7 @@ it('switches team members to their personal team', function () {
     expect(DeleteTeam::run($team))->toBeTrue()
         ->and($this->user->fresh()->currentTeam->is($this->user->personalTeam()))->toBeTrue()
         ->and($team->owner->currentTeam->is($team->owner->personalTeam()))->toBeTrue();
-})->skip();
+});
 
 it('does not delete personal teams', function () {
     expect(DeleteTeam::run($this->user->personalTeam()))->toBeFalse()
