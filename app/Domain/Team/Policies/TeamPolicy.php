c43d80a4d0dee6aa6c69f94962375cfe72717588
Adam Hainsworth-Potter <adam@moonpixels.co.uk>
2023-08-25 19:33:16 +0100

    Add team API endpoints

--- app/Domain/Team/Policies/TeamPolicy.php
+++ app/Domain/Team/Policies/TeamPolicy.php
@@ -11,12 +11,28 @@ class TeamPolicy
     use HandlesAuthorization;
 
     /**
+     * Determine whether the user can view any teams.
+     */
+    public function viewAny(User $user): bool
+    {
+        return true;
+    }
+
+    /**
      * Determine whether the user can view the team.
      */
     public function view(User $user, Team $team): bool
     {
         return $user->ownsTeam($team)
             || $user->belongsToTeam($team);
+    }
+
+    /**
+     * Determine whether the user can create teams.
+     */
+    public function create(User $user): bool
+    {
+        return true;
     }
 
     /**
