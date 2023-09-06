d90aa845848e73754a2d92de31feb07f80cab7ab
Adam Hainsworth-Potter <adam@moonpixels.co.uk>
2023-09-06 10:11:39 +0100

    Add landing page

--- resources/js/Pages/Auth/Partials/AuthHeader.vue
+++ resources/js/Pages/Auth/Partials/AuthHeader.vue
@@ -1,6 +1,10 @@
 <template>
   <div :class="[fullWidth ? '' : 'sm:max-w-sm']" class="sm:mx-auto sm:w-full">
-    <BlastLogo class="mx-auto h-10 w-auto" type="logomark" />
+    <Link href="/">
+      <span class="sr-only"> Blast home page </span>
+
+      <BlastLogo class="mx-auto h-10 w-auto" type="logomark" />
+    </Link>
 
     <h2 class="mt-6 text-center text-2xl font-semibold leading-9 tracking-tight text-zinc-900 dark:text-white">
       {{ title }}
@@ -14,6 +18,7 @@
 
 <script lang="ts" setup>
 import BlastLogo from '@/Components/Logo/BlastLogo.vue'
+import { Link } from '@inertiajs/vue3'
 
 type Props = {
   title: string
