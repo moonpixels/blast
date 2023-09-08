097347c601ba75ae074b28d08ae76db9cc7a5fc1
Adam Hainsworth-Potter <99101334+adamwhp@users.noreply.github.com>
2023-09-08 16:06:34 +0100

    Add site metadata (#117)
    Co-authored-by: adamwhp <adamwhp@users.noreply.github.com>

--- resources/js/components/AppHead.vue
+++ resources/js/components/AppHead.vue
@@ -4,6 +4,27 @@
       {{ title }}
     </title>
 
+    <meta content="Blast" name="apple-mobile-web-app-title" />
+    <meta content="Blast" name="application-name" />
+    <meta content="#09090b" name="theme-color" />
+    <meta content="website" name="og:type" />
+    <meta content="/images/social/social-card-large.jpg" name="og:image" />
+    <meta content="/images/social/social-card-large.jpg" name="twitter:image" />
+    <meta content="summary_large_image" name="twitter:card" />
+
+    <link href="/images/favicons/favicon-512x512.png?v=1" rel="icon" sizes="512x512" type="image/png" />
+    <link href="/images/favicons/favicon-256x256.png?v=1" rel="icon" sizes="256x256" type="image/png" />
+    <link href="/images/favicons/favicon-128x128.png?v=1" rel="icon" sizes="128x128" type="image/png" />
+    <link href="/images/favicons/favicon-64x64.png?v=1" rel="icon" sizes="64x64" type="image/png" />
+    <link href="/images/favicons/favicon-32x32.png?v=1" rel="icon" sizes="32x32" type="image/png" />
+    <link href="/images/favicons/apple-touch-icon.png?v=1" rel="apple-touch-icon" sizes="180x180" type="image/png" />
+
+    <meta
+      content="Privacy focused link shortening for developers. Blast allows you to easily integrate link shortening into your application, while keeping your users data private."
+      head-key="description"
+      name="description"
+    />
+
     <slot />
   </Head>
 </template>
@@ -20,6 +41,6 @@ type Props = {
 const props = defineProps<Props>()
 
 const title = computed<string>(() => {
-  return props.title ? `${props.title} - Blast` : trans('Blast - The privacy-focused open source URL shortener')
+  return props.title ? `${props.title} - Blast` : trans('Blast - The privacy focused URL shortener for developers')
 })
 </script>
